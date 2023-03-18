de34e015f8a89662c66c3d83983ec41a1dddc8d6
Mahmoud Hassan <126132249+Mahmoud-Hassan98@users.noreply.github.com>
2023-03-18 18:36:24 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Caffe-Z
+
+this  repository for Create a page using java
+
+The tools are:
+
+* vs code
+* figma
+* github
+
+ [live code  URL](https://mahmoud-hassan98.github.io/Portfolio/)
---
+++ app.js
@@ -0,0 +1,15 @@
+var name = prompt("Please enter your name:");
+var gender = prompt("Please enter your gender:");
+if (gender==="male" ) {
+    alert("welcome Mr " + name );
+}else if (gender==="female")
+{
+    alert("welcome Ms " +name );
+}else alert("welcome " + name );
+let drink = prompt("do you want a hot or a cold drink?")
+let drink_Name= prompt("name of the drink")
+alert("your drink is getting prepared");
+console.log(name + " ordered a " + drink + " "+drink_Name+ ".");
+
+
+
